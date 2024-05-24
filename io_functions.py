cfc77faaf61baa863fc91898b2a1cb1252809973
Lambda <Lambda@DESKTOP-0FNR0R2>
2024-05-24 23:49:58 +0200

    debug_nparray_audio_output

--- io_functions.py
+++ io_functions.py
@@ -1,11 +1,19 @@
 from scipy.io import wavfile
 from pathlib import Path
 import pretty_midi
+import pyaudio
 
 def read_wav(path):
     return wavfile.read(path)
 
 def read_midi(path):
     return pretty_midi.PrettyMIDI(path)
+
+
+def debug_play_np_array(np_array,wav_rate=44100):
+    stream = pyaudio.PyAudio().open(rate=wav_rate, format=pyaudio.paInt16, channels=int(np_array.shape[1]), output=True)
+    stream.write(np_array.tobytes())
+    stream.close() # this blocks until sound finishes playing
+    pyaudio.PyAudio().terminate()
 
 
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from io_functions import read_midi
+from io_functions import read_midi,read_wav,debug_play_np_array
 
 if __name__ == "__main__":
     print(read_midi("test_files/TOUHOU_-_Bad_Apple.mid").instruments)
+    rate,data = read_wav("test_files/never_gonna_test.wav")
+    debug_play_np_array(data,rate)
