212945b50f54864144058cb75b6d0482663c0c6e
Lambda <Lambda@DESKTOP-0FNR0R2>
2024-05-25 17:22:53 +0200

    functional Instruments

--- instruments.py
+++ instruments.py
@@ -2,7 +2,7 @@ import math
 import numpy as np
 import librosa
 
-class Instrument:
+class Instrument_nparray:
     base_freq = 440 #in Hz
     sample_rate = 44100
     data = np.array([])
@@ -20,5 +20,23 @@ class Instrument:
     def pitch_shift(self,to_freq):
         return np.transpose(librosa.effects.pitch_shift(y=np.transpose(self.data).astype(float),sr=self.sample_rate,n_steps=math.log2(to_freq/self.base_freq)*12.0,bins_per_octave=12)).astype(np.short)
     
-    def wave_func(self,x): #assume short enough time-frame to do global fft
+    def wave_func(self,x):
         return np.take(self.shifted,np.remainder(np.floor(16*x).astype(np.int32),self.shifted.shape[0]))
+    
+class Instrument_func:
+    base_freq = 440 #in Hz
+    sample_rate = 44100
+    func = np.sin
+    shifted = np.sin
+    
+    def __init__(self,func = np.sin,base_freq = 440,sample_rate = 44100):
+        self.func = func
+        self.base_freq = base_freq
+        self.sample_rate = sample_rate
+        self.shifted = self.pitch_shift(440)
+    
+    def pitch_shift(self,to_freq):
+        return (lambda x: self.func(to_freq/self.base_freq*x))
+    
+    def wave_func(self,x):
+        return self.shifted(x)
