b4d4991315bcb26de69f91e0466011a12d22845b
Lambda <Lambda@DESKTOP-0FNR0R2>
2024-05-26 03:41:15 +0200

    custom pygame objects

---
+++ custom_pygame_objects.py
@@ -0,0 +1,75 @@
+import pygame as pg
+
+class VerticalSlider(): #class to build a vertical Slider, the built-ins didn't work or weren't vertical
+    def __init__(self, parent, base_rect = pg.Rect(0,0,0,0), value = 0.0, value_range = (0.0,1.0), slider_radius = 10):
+        self.parent = parent
+        self.rect = base_rect
+        self.value = value
+        self.value_range = value_range
+        self.slider_radius = slider_radius
+    
+    def render(self,screen):
+        pg.draw.rect(screen, pg.Color("azure4"), self.rect, width=0)
+        x, y, w, h = self.rect
+        if self.value_range[1]-self.value_range[0] == 0:
+            pg.draw.circle(screen, pg.Color("azure3"), (x+w/2.0,y), self.slider_radius, width=0)
+        pg.draw.circle(screen, pg.Color("azure3"), (x+w/2.0,y+self.value/(self.value_range[1]-self.value_range[0])*h), self.slider_radius, width=0)
+    
+    def move_slider(self, mouse_pos):
+        x, y, w, h = self.rect
+        pos = mouse_pos[1]
+        #clamp the movement
+        if pos < y:
+            pos = y
+        elif pos > y+h:
+            pos = y+h
+        self.value = (self.value_range[1]-self.value_range[0])*(pos-y)/h
+    
+    def is_colliding(self, pos):
+        x, y, w, h = self.rect
+        cx,cy = (x+w/2.0,y+self.value/(self.value_range[1]-self.value_range[0])*h)
+        return self.rect.collidepoint(pos) or ((cx-pos[0])**2+(cy-pos[1])**2 <= self.slider_radius**2)
+
+
+class DropDown(): #dropdown menu
+    def __init__(self, parent, base_rect = pg.Rect(0,0,0,0), name = "", options=[]):
+        self.parent = parent
+        self.rect = base_rect
+        self.name = name
+        self.options = options
+        self.menu_activ = False
+        self.active_option = -1
+    
+    def render(self,screen):
+        pg.draw.rect(surf, "azure4", self.rect, width=0)
+        font = pg.font.Font(None, 32)
+        text = self.font.render(self.main, 1, (0, 0, 0))
+        screen.blit(text, text.get_rect(center = self.rect.center))
+        if self.menu_activ:
+            for i in len(self.options):
+                rect = self.rect.copy()
+                rect.y += (i+1) * self.rect.height
+                pg.draw.rect(screen, "azure3", rect, width=0)
+                option = self.font.render(self.options[i], 1, (0, 0, 0))
+                screen.blit(option, option.get_rect(center = rect.center))
+    
+    def event(self, event_list):#TODO: rewrite
+        mpos = pg.mouse.get_pos()
+        self.menu_active = self.rect.collidepoint(mpos)
+        
+        self.active_option = -1
+        for i in range(len(self.options)):
+            rect = self.rect.copy()
+            rect.y += (i+1) * self.rect.height
+            if rect.collidepoint(mpos):
+                self.active_option = i
+                break
+
+        for event in event_list:
+            if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+                if self.menu_active:
+                    self.draw_menu = not self.draw_menu
+                elif self.draw_menu and self.active_option >= 0:
+                    self.draw_menu = False
+                    return self.active_option
+        return -1
